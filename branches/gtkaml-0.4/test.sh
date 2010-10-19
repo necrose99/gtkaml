4c5b8190a3987b5b7c1dc1231e70dbb70da157fc
b100dian <b100dian@6bdb3c60-4643-0410-86bb-99cbaf4d9aa6>
2010-10-19 10:58:45 +0000

    improved testing

--- branches/gtkaml-0.4/test.sh
+++ branches/gtkaml-0.4/test.sh
@@ -1,1 +1,4 @@
-src/gtkamlc test.gtkaml --save-temps --pkg gtk+-2.0 --dump-tree test.vala ; cat test.vala
+rm test.vala
+touch test.vala
+src/gtkamlc test.gtkaml --save-temps --pkg gtk+-2.0 --dump-tree test.vala
+cat test.vala
